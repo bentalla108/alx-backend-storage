d7f2d9fc06c0302fbc34b286c9f88b8daced938a
bentalla108 <tallambaye108@gmail.com>
2023-07-13 10:24:46 +0000

    Contry by Country

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,9 @@
--- Create table 'user' if it doesn't already exist
+-- Drop table 'user' if it  already exist
 DROP TABLE IF EXISTS users;
-
+-- Create table 'user' if it doesn't already exist
 CREATE TABLE users(id INT PRIMARY KEY AUTO_INCREMENT, 
 email VARCHAR(256),
 name VARCHAR(256),
-country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
+country CHAR(2) NOT NULL DEFAULT 'US' 
+CHECK (country IN ('US', 'CO', 'TN'))
 );
