6491cf98b31de1321794fa618089affdad04c804
bentalla108 <tallambaye108@gmail.com>
2023-07-13 10:14:22 +0000

    Unique user

--- 0x00-MySQL_Advanced/0-uniqu_users.sql
+++ 0x00-MySQL_Advanced/0-uniqu_users.sql
@@ -1,5 +1,6 @@
 -- Create table 'user' if it doesn't already exist
-CREATE TABLE IF NOT EXISTS user (
+
+CREATE TABLE IF NOT EXISTS users (
     id INT PRIMARY KEY AUTO_INCREMENT, -- Primary key column, auto-incrementing
     email VARCHAR(256), -- Email column with a maximum length of 256 characters
     name VARCHAR(256) -- Name column with a maximum length of 256 characters
